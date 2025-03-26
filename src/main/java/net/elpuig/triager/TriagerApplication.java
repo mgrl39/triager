7fe5dfee6c6ba347229a690be9e4532f0e5484b8
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-03-26 17:37:16 +0100

    project initialization

---
+++ src/main/java/net/elpuig/triager/TriagerApplication.java
@@ -0,0 +1,13 @@
+package net.elpuig.triager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/elpuig/triager/TriagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.elpuig.triager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
