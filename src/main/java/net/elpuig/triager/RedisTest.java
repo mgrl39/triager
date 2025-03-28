d76169909db899ecd1cb061ebef7da19e25cdc4b
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-03-28 17:33:00 +0100

    add command line simple system + RedisConfiguration, RedisConenctionTest and RedisTest (not working yet)

--- src/main/java/net/elpuig/triager/RedisTest.java
+++ src/main/java/net/elpuig/triager/RedisTest.java
@@ -1,5 +1,10 @@
 package net.elpuig.triager;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPooled;
+
 public class RedisTest {
+
 
 }
--- src/main/java/net/elpuig/triager/TriagerApplication.java
+++ src/main/java/net/elpuig/triager/TriagerApplication.java
@@ -1,13 +1,39 @@
 package net.elpuig.triager;
 
+import net.elpuig.triager.config.MensajeApp;
+import net.elpuig.triager.config.RedisConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import static net.elpuig.triager.config.MensajeApp.colorInitializator;
 
 @SpringBootApplication
 public class TriagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TriagerApplication.class, args);
+
+        Map<String, String> colors = colorInitializator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(MensajeApp.BIENVENIDA.get());
+        while (true) {
+            System.out.print(colors.get("blue_bold") + MensajeApp.PROGRAMA.get() + colors.get("reset") + "$ ");
+            handleCommands(scanner.nextLine());
+        }
+    }
+
+    public static void handleCommands(String command)
+    {
+        if (command.equals("exit") || command.equals("quit")) System.exit(0);
+        else if (command.equals("info") || command.equals("help")) helpCommand();
+    }
+
+    public static void helpCommand() {
+        System.out.println(colorInitializator().get("yellow_bold") + "COMANDOS TRIAGER" + colorInitializator().get("reset") + "");
     }
 
 }
