4a19f75eabcce1b40c350cc669d5f64c315ac5e0
usuario <170405410+mgrl39@users.noreply.github.com>
2025-03-28 17:47:14 +0100

    refactor code

--- src/main/java/net/elpuig/triager/TriagerApplication.java
+++ src/main/java/net/elpuig/triager/TriagerApplication.java
@@ -1,11 +1,9 @@
 package net.elpuig.triager;
 
 import net.elpuig.triager.config.MensajeApp;
-import net.elpuig.triager.config.RedisConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -14,26 +12,32 @@ import static net.elpuig.triager.config.MensajeApp.colorInitializator;
 @SpringBootApplication
 public class TriagerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TriagerApplication.class, args);
+  static Scanner scanner = new Scanner(System.in);
 
-        Map<String, String> colors = colorInitializator();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(MensajeApp.BIENVENIDA.get());
-        while (true) {
-            System.out.print(colors.get("blue_bold") + MensajeApp.PROGRAMA.get() + colors.get("reset") + "$ ");
-            handleCommands(scanner.nextLine());
-        }
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(TriagerApplication.class, args);
 
-    public static void handleCommands(String command)
-    {
-        if (command.equals("exit") || command.equals("quit")) System.exit(0);
-        else if (command.equals("info") || command.equals("help")) helpCommand();
+    Map<String, String> colors = colorInitializator();
+    System.out.println(MensajeApp.BIENVENIDA.get());
+    while (true) {
+      System.out.print(colors.get("blue_bold") + MensajeApp.PROGRAMA.get() + colors.get("reset") + "$ ");
+      handleCommands(scanner.nextLine());
     }
+  }
 
-    public static void helpCommand() {
-        System.out.println(colorInitializator().get("yellow_bold") + "COMANDOS TRIAGER" + colorInitializator().get("reset") + "");
-    }
+  public static void handleCommands(String command) {
+    if (command.equals("exit") || command.equals("quit")) {
+      scanner.close();
+      System.exit(0);
+    } else if (command.equals("info") || command.equals("help"))
+      helpCommand();
+    else
+      System.out.print(MensajeApp.COMMAND_NOT_FOUND.get());
+  }
+
+  public static void helpCommand() {
+    System.out.println(colorInitializator()
+        .get("yellow_bold") + "COMANDOS TRIAGER" + colorInitializator().get("reset") + "");
+  }
 
 }
